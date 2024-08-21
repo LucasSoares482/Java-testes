9d6d1344a14c2ebe80f5a38c3d2c46bc04de24ab
Lucas Soares Santos (Padrinho) <99371201+Luquinhas482@users.noreply.github.com>
2024-08-21 22:59:44 +0000

    add: Código hello world em java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//cria uma classe para dizer hello world
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+//para compilar, utilize abaixo:
+//ls = javac HelloWorld.java = java HelloWorld
