bc9366824dfaf470c2674b45074d7a5c52c114c9
Lucas Soares Santos (Padrinho) <99371201+Luquinhas482@users.noreply.github.com>
2024-08-22 00:46:50 +0000

    add:codigo de atividade

--- atividade.java
+++ atividade.java
@@ -1,6 +1,18 @@
 class gastos{
     public static void main(String[] args) {
-        int idade;
+
+        //gastos de 3 meses inteiros
+        int gasto_janeiro;
+        gasto_janeiro =15000;
+        int gasto_fervereiro;
+        gasto_fervereiro = 23000;
+        int gasto_marco;
+        gasto_marco = 17000;
+
+        //trimestre resultado
+        int gasto_trimestre;
+        gasto_trimestre =gasto_janeiro+gasto_fervereiro+gasto_marco;
+        System.out.println(gasto_trimestre); 
     }
 }
 
