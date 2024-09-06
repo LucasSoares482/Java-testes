037d994e439a6ca15f0e22f4ff0da9fe9cf37b80
Luquinhas482 <99371201+LucasSoares482@users.noreply.github.com>
2024-09-06 17:22:48 -0300

    feat

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+public class PrimeiroPrograma {
+//esta é uma classe onde pode ter varios metodos
+    public static void main(String[] args) {
+    //Este é um metodo que esta dentro de uma classe
+        System.out.println("Olá, Mundo!");
+        //isso é para o sistema gerar uma mensagem de ola mundo.
+    }
+}
