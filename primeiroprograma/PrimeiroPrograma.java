b7d059885877f394f3524af21c52e918fe257d51
Luquinhas482 <99371201+LucasSoares482@users.noreply.github.com>
2024-09-06 19:20:37 -0300

    feat

--- primeiroprograma/PrimeiroPrograma.java
+++ primeiroprograma/PrimeiroPrograma.java
@@ -1,7 +1,9 @@
+package primeiroprograma;
+//pacotes podem possuir varias classes com varios metodos
 public class PrimeiroPrograma {
 //esta é uma classe onde pode ter varios metodos
     public static void main(String[] args) {
-    //Este é um metodo que esta dentro de uma classe
+    //Este é um metodo que esta dentro de uma classe 
         System.out.println("Olá, Mundo!");
         //isso é para o sistema gerar uma mensagem de ola mundo.
     }
